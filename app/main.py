14a13afac9e67a67d63cec23550f30dcf5d617a2
Debasish Bose <devbose2008@gmail.com>
2022-10-27 18:30:03 -0400

    Added CORS middleware code snippet

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from .routers import post, user, auth, vote
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = ['https://www.google.com']
+
+app.add_middleware(CORSMiddleware,
+                   allow_origins=origins,
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"]
+                   )
 
 app.include_router(post.router)
 app.include_router(user.router)
