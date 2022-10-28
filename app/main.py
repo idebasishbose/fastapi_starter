72c2c23264c32a2c3ce90c6ecc4c52e2e4a8b80e
Debasish Bose <devbose2008@gmail.com>
2022-10-27 20:12:41 -0400

    Added root to main

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from .routers import post, user, auth, vote
 from fastapi.middleware.cors import CORSMiddleware
-
+# from .database import engine
+# from . import models
+#
+# models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 origins = ['https://www.google.com']
 
@@ -16,3 +19,8 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello"}
