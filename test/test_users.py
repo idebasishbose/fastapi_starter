70a3031bfbadae97489af9585f0b7d45bf26641c
Debasish Bose <devbose2008@gmail.com>
2022-10-27 21:14:57 -0400

    Added a fixture to automatically drop and create table before the create_user test runs

--- test/test_users.py
+++ test/test_users.py
@@ -5,13 +5,15 @@ from app.config import settings
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.database import get_db, Base
+import pytest
 
 # POstgres test database credentials
 SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
 # creating the test database
-Base.metadata.create_all(bind=engine)
 
 
 def override_get_db():
@@ -23,15 +25,21 @@ def override_get_db():
 
 
 app.dependency_overrides[get_db] = override_get_db
-client = TestClient(app)
 
 
-def test_root():
+@pytest.fixture
+def client():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    yield TestClient(app)
+
+
+def test_root(client):
     res = client.get("/")
     print(res.json())
 
 
-def test_create_user():
+def test_create_user(client):
     res = client.post("/users", json={"email": "hello1234@gmail.com", "password": "password123"})
     new_user = schemas.UserOut(**res.json())
     assert new_user.email == "hello1234@gmail.com"
