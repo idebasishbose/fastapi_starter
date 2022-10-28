6d0db880f77c7832b72055c7f0ff267c210bb58f
Debasish Bose <devbose2008@gmail.com>
2022-10-28 15:00:51 -0400

    Refactored all the fixtures into conftest.py. - All the test classes will have auto access to theses fixture defined in conftest

--- test/conftest.py
+++ test/conftest.py
@@ -34,3 +34,12 @@ def client(session):
     app.dependency_overrides[get_db] = override_get_db
 
     yield TestClient(app)
+
+@pytest.fixture()
+def test_user(client):
+    user_data = {"email": "hello1234@gmail.com", "password": "password123"}
+    res = client.post("/users", json=user_data)
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
--- test/test_users.py
+++ test/test_users.py
@@ -1,16 +1,5 @@
 from app import schemas
-from .database import client, session
 import pytest
-
-
-@pytest.fixture()
-def test_user(client):
-    user_data = {"email": "hello1234@gmail.com", "password": "password123"}
-    res = client.post("/users", json=user_data)
-    assert res.status_code == 201
-    new_user = res.json()
-    new_user['password'] = user_data['password']
-    return new_user
 
 
 def test_create_user(client):
